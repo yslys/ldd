76b08063d174dc841116f3236d9230611e597b37
yusen <liuys1997@gmail.com>
2021-03-21 00:47:46 -0500

    for kernel version adaptability

---
+++ scullp/access_ok_version.h
@@ -0,0 +1,7 @@
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
+#define access_ok_wrapper(type,arg,cmd) access_ok(type, arg, cmd)
+#else
+#define access_ok_wrapper(type,arg,cmd) access_ok(arg, cmd)
+#endif
